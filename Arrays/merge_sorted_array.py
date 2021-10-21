d2b49edb902fa24e147a42dafefef89ac32445d2
Irene Joseph <168341@ust.com>
2021-10-21 13:43:18 +0530

    code for merging soted arrays

--- Arrays/merge_sorted_array.py
+++ Arrays/merge_sorted_array.py
@@ -1,1 +1,14 @@
+def merge_sorted_array(array1,array2):
+    if len(array1) == 0:
+        return array2
+    if len(array2) == 0:
+        return array1
+    for each in array2:
+        array1.append(each)
+    array1.sort()
+    return array1
 
+
+
+
+print(merge_sorted_array([1,5,6],[3,4]))
