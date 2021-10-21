9f12bd6e626eeafe6d72dc4ce8ffdcffb35487c3
Irene Joseph <168341@ust.com>
2021-10-21 13:07:27 +0530

    code for reversing a string

--- Arrays/reverse_string.py
+++ Arrays/reverse_string.py
@@ -2,13 +2,23 @@ def reverse(inputstring):
     return inputstring[::-1]
 
 def reverse2(inputstring):
-    reversedstring = ""
+    reversedstring = []
     i = len(inputstring) - 1
     while i >= 0:
-        reversedstring += inputstring[i]
+        reversedstring.append(inputstring[i])
         i -= 1
-    return reversedstring
+    return ''.join(reversedstring)
 
+def reverse3(inputstring):
+    first = 0
+    last = len(inputstring) - 1
+    inputstring = list(inputstring)
+    while first < last:
+        inputstring[first], inputstring[last] = inputstring[last], inputstring[first]
+        first+=1
+        last-=1
+    return ''.join(inputstring)
 
 print(reverse("hello"))
 print(reverse2("hello"))
+print(reverse3("hello"))
