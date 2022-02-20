5ae55a3ca36e1d1f1b83a0dbfed83d6db573f894
Irene Joseph <33227567+irene-joseph@users.noreply.github.com>
2022-02-20 21:35:18 +0530

    Bubble sort algorithm implementation

--- Sorting_Algorithms/BubbleSort.js
+++ Sorting_Algorithms/BubbleSort.js
@@ -1,6 +1,6 @@
 // Bubble Sort
-// Runtime: 0( n2 ) average and worst case. 
-// Memory: 0( 1).
+// Runtime: 0(n^2) average and worst case. 
+// Memory: 0(1).
 /*In bubble sort, we start at the beginning of the array and swap the first two elements if the first is greater
 than the second. Then, we go to the next pair, and so on, continuously making sweeps of the array until it is
 sorted. In doing so, the smaller items slowly"bubble" up to the beginning of the list.*/
