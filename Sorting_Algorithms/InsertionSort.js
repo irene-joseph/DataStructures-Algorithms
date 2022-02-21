7cbfeaa36043edca3e1a05e6c9aa6eaeacacea08
Irene Joseph <33227567+irene-joseph@users.noreply.github.com>
2022-02-21 22:32:48 +0530

    Insertion sort implementation

---
+++ Sorting_Algorithms/InsertionSort.js
@@ -0,0 +1,25 @@
+/*Insertion sort is the sorting mechanism where the sorted array is built having one item at a time. 
+The array elements are compared with each other sequentially and then arranged simultaneously in some particular order.*/
+
+/*splice syntax : array.splice(index, howmany, item1, ....., itemX)
+return value of splice function is an array of the removed elements.*/
+
+function insertionSort(array) {
+  const arrlength = array.length;
+	for (let i = 0; i < arrlength; i++) {
+		if (array[i] < array[0]) {
+      //move number to the first position
+      array.unshift(array.splice(i,1)[0]);
+    } else {
+      if (array[i] < array[i-1]) {
+        //to find the exact position
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j-1] && array[i] < array[j]) {
+            //move number to the exact position
+            array.splice(j,0,array.splice(i,1)[0]); 
+          }
+        }
+      }
+    }
+	}
+}
