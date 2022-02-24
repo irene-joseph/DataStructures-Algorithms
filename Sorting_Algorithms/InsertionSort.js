fa05d5c1f4d8c14a36c21dcad165723de0ad8cb8
Irene Joseph <33227567+irene-joseph@users.noreply.github.com>
2022-02-24 19:26:38 +0530

    Insertion Sort Algorithm Implementation

--- Sorting_Algorithms/InsertionSort.js
+++ Sorting_Algorithms/InsertionSort.js
@@ -15,14 +15,12 @@ function insertionSort(array) {
       //move number to the first position
       array.unshift(array.splice(i,1)[0]);
     } else {
-      if (array[i] < array[i-1]) {
         //to find the exact position
         for (var j = 1; j < i; j++) {
           if (array[i] >= array[j-1] && array[i] < array[j]) {
             //move number to the exact position
             array.splice(j,0,array.splice(i,1)[0]); 
           }
-        }
       }
     }
   }
